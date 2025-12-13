2c98d1f78d7e708661de8494a469639726bb43e8
amadeus-faux <rey.13@upi.edu>
2025-12-13 23:00:59 +0700

    Add files via upload

--- auth/logout.php
+++ auth/logout.php
@@ -1,6 +1,15 @@
 <?php
+header('Content-Type: application/json; charset=utf-8');
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : 'null';
+header("Access-Control-Allow-Origin: $origin");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit;
+}
+
 session_start();
 $_SESSION = [];
 session_destroy();
-header('Content-Type: application/json; charset=utf-8');
 echo json_encode(['success' => true, 'message' => 'Logged out']);
